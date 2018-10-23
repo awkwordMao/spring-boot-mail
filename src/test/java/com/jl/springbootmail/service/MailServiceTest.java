b0cc71a224708dcf6184ec5fffa264eccab91aee
李杰 <2624824150@qq.com>
2018-10-23 17:38:29 +0800

    完结

--- src/test/java/com/jl/springbootmail/service/MailServiceTest.java
+++ src/test/java/com/jl/springbootmail/service/MailServiceTest.java
@@ -2,21 +2,41 @@ package com.jl.springbootmail.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.annotation.Resource;
-
-import static org.junit.Assert.*;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class MailServiceTest {
+
+    @Resource
+    MailService mailService;
+
+    @Resource
+    TemplateEngine templateEngine;
+
+    @Test
+    public void sendTemplateMailTest() throws Exception{
+        Context context = new Context();
+        context.setVariable("id", 007);
+
+        String emailTemplate = templateEngine.process("email", context);
+
+        mailService.sendHtmlMail("2624824150@qq.com", "模板邮件", emailTemplate);
+    }
+
     @Test
     public void sendInlineResourceMailTest() throws Exception {
         String imgPath = "C:/Users/007/Pictures/Camera Roll/timg.jpg";
         String rscId = "neo001";
-
+        String content = "<html>\n<body>\n" +
+                "这是有图片的邮件:<img src=\'cid:" + rscId
+                + "\'></img>" +
+                "</body></html>";
+        mailService.sendInlineResourceMail("2624824150@qq.com", "图片邮件", content, imgPath, rscId);
     }
 
     @Test
@@ -35,9 +55,6 @@ public class MailServiceTest {
 
         mailService.sendHtmlMail("2624824150@qq.com", "第一封邮件",content);
     }
-
-    @Resource
-    MailService mailService;
 
     @Test
     public void sendSimpleMailTest() throws Exception {
